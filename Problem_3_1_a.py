be07af727cb43b4e2ff8e9ca7a256d779294cefb
Noah Johnson <noah.johnson13@ncf.edu>
2016-04-26 18:12:33 -0400

    First commit, with dirichlet wave equation code. Doesn't seem to be working just yet, but the animation code is, and you should feel free to copy it.

---
+++ Problem_3_1_a.py
@@ -0,0 +1,114 @@
+'''
+author: Noah
+
+Dirichlet wave equation!
+u_tt = u_xx
+'''
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.animation as animation
+
+dt = 0.5
+dx = 1.0
+c = 1
+s = c**2 * dt**2 / dx**2
+
+x_start, x_end = 0.0, 5.0  # x-bounds
+t_start, t_end = 0.0, 3.0  # t-bounds
+
+X = np.arange(x_start, x_end + dx, dx)  # 1D x dimension
+T = np.arange(t_start, t_end + dt, dt)  # 1D t dimension
+
+J, N = X.shape[0], T.shape[0] # number of discrete x-axis and t-axis elements
+print J, N
+
+
+def phi_j(j): #initial condition: u(x,0)
+    return 25 - (j*dx)**2
+
+def psi_j(j): #initial velocity: du/dt(x,0)
+    return 0
+
+
+def initial_u(X, T):
+    """
+    Creates initial condition of wave equation
+    """
+    u = np.zeros((N,J), dtype='float')
+
+    # the first row
+    for j in range(J):   
+        u[0,j] = phi_j(j)
+
+    # the second row
+    for j in range(1,J-1):  # exclude edge points due to (j-1) and (j+1) terms, which enforces dirichlet condition
+        u[1,j] = (s/float(2)) * (phi_j(j+1) + phi_j(j-1)) + (1-s)*phi_j(j) + psi_j(j)*dt
+    
+    return u
+
+u = initial_u(X,T)
+
+# plot first two rows to confirm initial conditions
+fig = plt.figure()
+
+ax1 = fig.add_subplot(211)
+ax1.set_title("Initial condition")
+ax1.plot(u[0])
+ax2 = fig.add_subplot(212)
+ax2.set_title("n = 2")
+ax2.plot(u[1])
+
+plt.show()
+'''
+size = 10
+plt.figure(figsize=(size, size))
+plt.plot(X, u[:,0])
+plt.xlim(xmin=x_start, xmax=x_end)
+plt.ylim(ymin=0)
+plt.title('initial condition')
+plt.show()'''
+#pyplot.savefig("test_0.png")
+
+def picard_engage(u, s, J, N):
+    """Implements a simple scheme for solving the diffusion equation for
+    the Dirichlet Boundary conditions: u(t, 0) = 0 and u(t, l) = 0.
+
+    Arguments
+    ---------
+    u -- wave solution, with first two rows already filled in.
+    s -- ratio of step distances between points in t, and between points in x
+    J -- total number of discrete x-axis points
+    N -- total number of discrete t-axis points
+    """
+    for n in range(1,N-1):
+        for j in range(1, J - 1):
+            u[n+1,j] = s*(u[n,j+1] + u[n,j-1]) + 2*(1-s)*u[n,j] - u[n-1,j]
+
+        #enforce boundary conditions
+        u[n+1,0] = 0
+        u[n+1,J-1] = 0
+    
+    return u
+
+u = picard_engage(u, s, J, N)
+
+# plot animation of solution as time elapses
+fig, ax = plt.subplots()
+ax.set_xlabel('X')
+ax.set_ylabel('u')
+wave, = ax.plot(X, u[0,:])
+
+def animate(i):
+    ax.set_title("n = %d" % i)
+    wave.set_ydata(u[i,:])  # update the data
+    return wave,
+
+
+# Init only required for blitting to give a clean slate.
+def init():
+    wave.set_ydata(u[0,:])
+    return wave,
+
+ani = animation.FuncAnimation(fig, animate, N, init_func=init,
+                              interval=10000/N, blit=False, repeat=False)
+plt.show()
